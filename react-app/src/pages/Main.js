90c9d060f21012a01ee6f0e6faedbff9c1e3a890
fdsa20040200 <fdsa200@naver.com>
2022-09-26 13:37:38 +0900

    add styled-components module

---
+++ react-app/src/pages/Main.js
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+const StyledMain = styled.div({
+    color:'red',
+})
+
+function Main(){
+    return(<StyledMain>
+        a
+    </StyledMain>)
+}
+
+export default Main;
